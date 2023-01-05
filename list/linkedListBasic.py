25e584799423a45812bb79b7eb8169d61db9ccd8
rayark1 <max980203@snu.ac.kr>
2023-01-05 17:00:14 +0900

    feat(list/linkedListBasic.py): 구현할 함수 기능 목록 추가

--- list/linkedListBasic.py
+++ list/linkedListBasic.py
@@ -1,2 +1,40 @@
 # import the ListNode class
 from dataStructure.list.listNode import ListNode
+
+
+class LinkedListBasic:
+    def __init__(self):
+        pass
+
+    def insert():
+        pass
+
+    def append():
+        pass
+
+    def pop():
+        pass
+
+    def remove():
+        pass
+
+    def index():
+        pass
+
+    def clear():
+        pass
+
+    def count():
+        pass
+
+    def reverse():
+        pass
+
+    def sort():
+        pass
+
+    def copy():
+        pass
+
+    def extend():
+        pass
