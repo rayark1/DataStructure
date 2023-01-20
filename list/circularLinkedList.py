9340e28ef991c06d9e6d110a9edba355cbb38f62
rayark1 <max980203@snu.ac.kr>
2023-01-20 21:55:29 +0900

    feat(list/circularLinkedList.py): Add iterator

--- list/circularLinkedList.py
+++ list/circularLinkedList.py
@@ -9,3 +9,27 @@ class CircularLinkedList:
         self.__tail = ListNode('dummy', None)
         self.__tail.next = self.__tail
         self.__numItem = 0
+
+    def __iter__(self):
+        """Returns the list's iterator for traversing the list."""
+
+        return CircularLinkedListIterator(self.__tail)
+
+
+class CircularLinkedListIterator:
+    """This class implements an iterator for a circular linked list."""
+
+    def __init__(self, tail):
+        self.__tail = tail
+        self.__currNode = tail.next
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.__currNode == self.__tail:
+            raise StopIteration
+        else:
+            item = self.__currNode.item
+            self.__currNode = self.__currNode.next
+            return item
